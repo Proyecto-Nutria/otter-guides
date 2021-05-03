10a051faafaab35391d4e81266ae8543b0242396
searleser97 <gabrielsanv97@gmail.com>
2021-05-03 00:11:28 -0500

    updated on 05/03/2021 at 12:11:28 AM

---
+++ docusaurus.config.js
@@ -0,0 +1,59 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "otter-guides",
+  tagline: "otter-guides",
+  url: "https://proyecto-nutria.io",
+  baseUrl: "/otter-guides/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "proyecto-nutria", // Usually your GitHub org/user name.
+  projectName: "otter-guides", // Usually your repo name.
+  themeConfig: {
+    colorMode: {
+      defaultMode: "dark",
+    },
+    navbar: {
+      title: "otter-guides",
+      items: [
+        {
+          type: "doc",
+          position: "left",
+          docId: "about",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/proyecto-nutria/otter-guides",
+          position: "right",
+          className: "header-github-link",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      copyright: `Copyright © ${new Date().getFullYear()} Proyecto Nutria. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
