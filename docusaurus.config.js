90b71a451b06ef009818206614318e2f2e5cc860
Manuel CH <manuel_7795@hotmail.com>
2021-08-20 23:19:32 -0500

    Setup new section YAOS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,14 +1,14 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "otter-guides",
-  tagline: "otter-guides",
-  url: "https://proyecto-nutria.io",
-  baseUrl: "/otter-guides/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "proyecto-nutria",
-  projectName: "otter-guides",
+  title: 'otter-guides',
+  tagline: 'otter-guides',
+  url: 'https://proyecto-nutria.io',
+  baseUrl: '/otter-guides/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'proyecto-nutria',
+  projectName: 'otter-guides',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
@@ -23,34 +23,40 @@ module.exports = {
   },
   themeConfig: {
     colorMode: {
-      defaultMode: "dark",
+      defaultMode: 'dark',
     },
     navbar: {
-      title: "otter-guides",
+      title: 'otter-guides',
       logo: {
         alt: 'Proyecto Nutria Logo',
         src: 'img/logo.svg',
       },
       items: [
         {
-          type: "doc",
-          position: "left",
-          docId: "interview/about",
-          label: "Interview",
+          type: 'doc',
+          position: 'left',
+          docId: 'interview/about',
+          label: 'Interview',
+        },
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'yaos/about',
+          label: 'YAOS',
         },
         {
           type: 'localeDropdown',
           position: 'right',
         },
         {
-          href: "https://github.com/proyecto-nutria/otter-guides",
-          position: "right",
-          className: "header-github-link",
+          href: 'https://github.com/proyecto-nutria/otter-guides',
+          position: 'right',
+          className: 'header-github-link',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       logo: {
         alt: 'Proyecto Nutria Logo',
         src: 'img/icon-70x70.png',
@@ -61,20 +67,20 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
           path: './docs',
           routeBasePath: '/',
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/proyecto-nutria/otter-guides",
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/proyecto-nutria/otter-guides',
         },
         blog: {
           showReadingTime: true,
-          editUrl: "https://github.com/proyecto-nutria/otter-guides",
+          editUrl: 'https://github.com/proyecto-nutria/otter-guides',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
